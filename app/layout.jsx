7ff334f16557e27bf46760d4893f82793ab39a01
Eren AVAR <eren.avar@hotmail.com>
2025-03-18 20:47:24 +0300

    add metadata and favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import "@/assets/styles/global.css";
 
+export const metadata = {
+  title: "Rent Your Home",
+  keywords: "rent,real estate,home",
+  description: "Find your perfect home",
+};
+
 const MainLayout = ({ children }) => {
   return (
     <html>
