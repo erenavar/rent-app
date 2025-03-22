a7f5180cabd22b7cf6638b0eedab678fb67a9617
Eren AVAR <eren.avar@hotmail.com>
2025-03-22 13:30:58 +0300

    add props and their values to infobox

--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -6,8 +6,28 @@ const InfoBoxes = () => {
     <section>
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-          <InfoBox />
-          <InfoBox />
+          <InfoBox
+            heading="For Renters"
+            buttonInfo={{
+              text: "Browse Peoperties",
+              link: "/properties",
+              backgroundColor: "bg-black",
+            }}>
+            {" "}
+            Find your dream rental property. Bookmark properties and contact
+            owners.
+          </InfoBox>
+          <InfoBox
+            heading="For Property Owner"
+            backgroundColor="bg-blue-100"
+            buttonInfo={{
+              text: "Add Property",
+              link: "/properties/add",
+              backgroundColor: "bg-blue-500",
+            }}>
+            List your properties and reach potential guests. Rent as an Airbnb
+            or long term.
+          </InfoBox>
         </div>
       </div>
     </section>
