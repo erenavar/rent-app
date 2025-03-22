1a2b6eeaca47ad83e14919d0929740efa67a97b0
Eren AVAR <eren.avar@hotmail.com>
2025-03-22 21:10:22 +0300

    limit recent properties on home page

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -2,6 +2,7 @@ import properties from "@/properties.json";
 import PropertyCard from "./PropertyCard";
 
 const HomeProperties = () => {
+  const recentProperties = properties.slice(0, 3);
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
@@ -12,7 +13,7 @@ const HomeProperties = () => {
           <p>No Properties Found</p>
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
+            {recentProperties.map((property) => (
               <PropertyCard key={property._id} property={property} />
             ))}
           </div>
