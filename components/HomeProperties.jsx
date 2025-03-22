e44147087912c316eed0c58a33dc1a7bcf22b701
Eren AVAR <eren.avar@hotmail.com>
2025-03-22 21:07:53 +0300

    create home properties and show it on home page

---
+++ components/HomeProperties.jsx
@@ -0,0 +1,25 @@
+import properties from "@/properties.json";
+import PropertyCard from "./PropertyCard";
+
+const HomeProperties = () => {
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
+          Recent Properties
+        </h2>
+        {properties.length === 0 ? (
+          <p>No Properties Found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default HomeProperties;
