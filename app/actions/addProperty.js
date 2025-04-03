fdc410723f2ab07d0e7eb8e61f8cddbdaefcc700
Eren AVAR <eren.avar@hotmail.com>
2025-04-03 14:22:26 +0300

    fetch user id and it assigned to owner

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -1,7 +1,20 @@
 "use server";
 
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
 async function addProperty(formData) {
+  await connectDB();
+  const sessionUser = await getSessionUser();
+  if (!session == !session.userId) {
+    throw new Error("User ID is required.");
+  }
+  const { userId } = sessionUser;
   const propertyData = {
+    ownser: userId,
     type: formData.get("type"),
     name: formData.get("name"),
     description: formData.get("description"),
