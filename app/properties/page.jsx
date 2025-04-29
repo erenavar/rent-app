3c31a7de993678395bcaa10877ec99668fd21b42
Eren AVAR <eren.avar@hotmail.com>
2025-04-29 23:52:27 +0300

    fix property.map error

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -4,17 +4,21 @@ import Property from "@/models/Property";
 
 const PropertiesPage = async () => {
   await connectDB();
-  const properties = Property.find({}).lean();
+
+  const properties = await Property.find({}).lean();
+
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? (
+        {properties && properties.length === 0 ? (
           <p>No Properties Found</p>
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
+            {properties &&
+              Array.isArray(properties) &&
+              properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
           </div>
         )}
       </div>
