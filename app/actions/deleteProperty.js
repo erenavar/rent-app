89d1c1b844903b1bf45fea032abd2486827617e6
Eren AVAR <eren.avar@hotmail.com>
2025-04-30 13:56:47 +0300

    fetch user data for delete action

--- app/actions/deleteProperty.js
+++ app/actions/deleteProperty.js
@@ -1,5 +1,14 @@
 "use server";
 
-const deleteProperty = async (propertyId) => {};
+import { getSessionUser } from "@/utils/getSessionUser";
+
+const deleteProperty = async (propertyId) => {
+  const sessionUser = await getSessionUser;
+
+  if (!getSessionUser || sessionUser.userId) {
+    throw new Error("User Id is required.");
+  }
+  const { userId } = sessionUser;
+};
 
 export default deleteProperty;
