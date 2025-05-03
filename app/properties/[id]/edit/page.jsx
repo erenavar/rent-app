04e97a278018e067f2e41795aae822bdcc0c74f1
Eren AVAR <eren.avar@hotmail.com>
2025-05-03 21:41:10 +0300

    fetch data from db to edit page and fix maximum call stack size exceeded error

--- app/properties/[id]/edit/page.jsx
+++ app/properties/[id]/edit/page.jsx
@@ -1,5 +1,22 @@
-const PropertyEditPage = () => {
-  return <div>Edit</div>;
+import PropertyEditForm from "@/components/PropertyEditForm";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { convertToSerializableObject } from "@/utils/convertToObject";
+
+const PropertyEditPage = async ({ params }) => {
+  await connectDB();
+  const propertyDoc = Property.findById(params.id).lean();
+  const property = convertToSerializableObject(propertyDoc);
+  console.log(property);
+  return (
+    <section className="bg-blue-50">
+      <div className="container m-auto max-w-2xl py-24">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0 ">
+          <PropertyEditForm />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PropertyEditPage;
--- utils/convertToObject.js
+++ utils/convertToObject.js
@@ -1,28 +1,43 @@
 import { Types } from "mongoose";
 
-export function convertToSerializableObject(obj) {
+export function convertToSerializableObject(obj, visited = new Set()) {
   if (obj === null || typeof obj !== "object") {
     return obj;
   }
 
+  if (visited.has(obj)) {
+    return "[Circular]";
+  }
+
+  visited.add(obj);
+
   if (obj instanceof Date) {
+    visited.delete(obj);
     return obj.toISOString();
   }
 
   if (obj instanceof Types.ObjectId) {
+    visited.delete(obj);
     return obj.toString();
   }
 
   if (Array.isArray(obj)) {
-    return obj.map((item) => convertToSerializableObject(item));
+    const serializableArray = obj.map((item) =>
+      convertToSerializableObject(item, visited)
+    );
+
+    visited.delete(obj);
+    return serializableArray;
   }
 
   const serializableObj = {};
   for (const key in obj) {
     if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      serializableObj[key] = convertToSerializableObject(obj[key]);
+      serializableObj[key] = convertToSerializableObject(obj[key], visited);
     }
   }
+
+  visited.delete(obj);
 
   return serializableObj;
 }
