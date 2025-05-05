dc46a449f625d6da07695e4d56c44310c99234e4
Eren AVAR <eren.avar@hotmail.com>
2025-05-05 23:30:05 +0300

    coonect to db and get user session and its control construction

--- app/actions/updateProperty.js
+++ app/actions/updateProperty.js
@@ -1,5 +1,21 @@
 "use server";
 
-async function updateProperty() {}
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+async function updateProperty(propertyId, formData) {
+  await connectDB();
+
+  const sessionUser = await getSessionUser();
+
+  const { userId } = sessionUser;
+
+  const existingProperty = await Property.findById(propertyId);
+
+  if (existingProperty.owner.toString() !== userId) {
+    throw new Error("Current user isn't own this property");
+  }
+}
 
 export default updateProperty;
