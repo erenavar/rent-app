8d905e422e3d384e8aa4587f0d38a732835959bd
Eren AVAR <eren.avar@hotmail.com>
2025-05-06 22:49:48 +0300

    completed map functions and fetch lat-long

--- components/PropertyMap.jsx
+++ components/PropertyMap.jsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useState } from "react";
-import { setDefaults } from "react-geocode";
+import { useEffect, useState } from "react";
+import { fromAddress, setDefaults } from "react-geocode";
 
 const PropertyMap = ({ property }) => {
   const [lat, setLat] = useState(null);
@@ -22,6 +22,28 @@ const PropertyMap = ({ property }) => {
     language: "en",
     region: "nl",
   });
+
+  useEffect(() => {
+    const fetchCoords = async () => {
+      try {
+        const res = await fromAddress(
+          `${property.location.street} ${property.location.city} ${property.location.state} ${property.location.zipcode}`
+        );
+        if (res.results.length === 0) {
+          setGeocodeError(true);
+          return;
+        }
+        const { lat, long } = res.results[0].geometry.location;
+        console.log(lat, lang);
+      } catch (error) {
+        console.log("Fetching Coords: " + error);
+        setGeocodeError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCoords();
+  }, []);
 
   return <div>Map</div>;
 };
