71911e7c64763343040193062eb047112ae24d21
Eren AVAR <eren.avar@hotmail.com>
2025-05-06 22:31:00 +0300

    create variables for map

--- components/PropertyMap.jsx
+++ components/PropertyMap.jsx
@@ -1,6 +1,28 @@
 "use client";
 
+import { useState } from "react";
+import { setDefaults } from "react-geocode";
+
 const PropertyMap = ({ property }) => {
+  const [lat, setLat] = useState(null);
+  const [long, setLong] = useState(null);
+  const [viewport, setViewport] = useState({
+    latiutude: 0,
+    longitude: 0,
+    zoom: 12,
+    height: "100%",
+    width: "500px",
+  });
+
+  const [loading, setLoading] = useState(true);
+  const [geocodeError, setGeocodeError] = useState(false);
+
+  setDefaults({
+    key: process.env.NEXT_PUBLIC_GOOGLE_GEOCODE_API_KEY,
+    language: "en",
+    region: "nl",
+  });
+
   return <div>Map</div>;
 };
 
